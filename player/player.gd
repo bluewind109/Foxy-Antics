f3c11ea6863a9663f59273a7e6911bd2574c2c8e
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2024-04-20 11:21:30 +0700

    Add player state

--- player/player.gd
+++ player/player.gd
@@ -11,6 +11,9 @@ const MAX_FALL_SPEED: float = 400.0
 const HURT_TIME: float = 0.3
 const JUMP_VELOCITY: float = -400.0
 
+enum PLAYER_STATE { IDLE, RUN, JUMP, FALL, HURT }
+var _state = PLAYER_STATE.IDLE
+
 func _ready():
 	pass
 	
@@ -19,16 +22,18 @@ func _physics_process(delta):
 		velocity.y += GRAVITY * delta
 		
 	get_input()	
-		
 	move_and_slide()
+	calculate_states()
 
 func get_input() -> void:
 	velocity.x = 0
 	
 	if (Input.is_action_pressed("left") == true):
 		velocity.x = -RUN_SPEED
+		sprite_2d.flip_h = true
 	elif (Input.is_action_pressed("right") == true):
 		velocity.x = RUN_SPEED
+		sprite_2d.flip_h = false
 		
 	if (Input.is_action_just_pressed("jump") == true
 	and 
@@ -36,3 +41,36 @@ func get_input() -> void:
 		velocity.y = JUMP_VELOCITY
 		
 	velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL_SPEED)
+
+func calculate_states() -> void:
+	if (_state == PLAYER_STATE.HURT):
+		return
+		
+	if (is_on_floor() == true):
+		if (velocity.x == 0):
+			set_state(PLAYER_STATE.IDLE)
+		else:
+			set_state(PLAYER_STATE.RUN)
+	else:
+		if (velocity.y > 0):
+			set_state(PLAYER_STATE.FALL)
+		else:
+			set_state(PLAYER_STATE.JUMP)
+
+func set_state(new_state: PLAYER_STATE) -> void:
+	if (new_state == _state):
+		return
+		
+	_state = new_state
+	
+	match _state:
+		PLAYER_STATE.IDLE:
+			animation_player.play("idle")
+		PLAYER_STATE.RUN:
+			animation_player.play("run")
+		PLAYER_STATE.JUMP:
+			animation_player.play("jump")
+		PLAYER_STATE.FALL:
+			animation_player.play("fall")
+			
+	

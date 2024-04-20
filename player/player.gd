f9e0da1fda3087ea2f9240b105aaf2b76f2fbcc1
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2024-04-20 11:02:20 +0700

    Add player movement

--- player/player.gd
+++ player/player.gd
@@ -6,6 +6,10 @@ class_name Player
 @onready var animation_player = $AnimationPlayer
 
 const GRAVITY: float = 1000.0
+const RUN_SPEED: float = 120.0
+const MAX_FALL_SPEED: float = 400.0
+const HURT_TIME: float = 0.3
+const JUMP_VELOCITY: float = -400.0
 
 func _ready():
 	pass
@@ -14,4 +18,21 @@ func _physics_process(delta):
 	if (is_on_floor() == false):
 		velocity.y += GRAVITY * delta
 		
+	get_input()	
+		
 	move_and_slide()
+
+func get_input() -> void:
+	velocity.x = 0
+	
+	if (Input.is_action_pressed("left") == true):
+		velocity.x = -RUN_SPEED
+	elif (Input.is_action_pressed("right") == true):
+		velocity.x = RUN_SPEED
+		
+	if (Input.is_action_just_pressed("jump") == true
+	and 
+	is_on_floor() == true):
+		velocity.y = JUMP_VELOCITY
+		
+	velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL_SPEED)

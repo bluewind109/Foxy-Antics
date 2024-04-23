56ec8243981a6ad4f1a6803e748d40e2cbf70377
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2024-04-23 11:52:58 +0700

    Prevent boss from getting damaged before his entrance animation finished

--- boss/boss.gd
+++ boss/boss.gd
@@ -5,15 +5,17 @@ const HIT_CONDITION: String = "parameters/conditions/on_hit"
 
 @onready var animation_tree = $AnimationTree
 @onready var visual = $Visual
+@onready var hit_box = $Visual/HitBox
 
 @export var lives: int = 2
 @export var points: int = 5
 
 var _invincible: bool = false
+var _has_arrived: bool = false
 
 
 func _ready():
-	pass
+	hit_box.monitoring = false
 
 
 func tween_hit() -> void:
@@ -35,11 +37,21 @@ func set_invincible(val: bool) -> void:
 
 
 func take_damage() -> void:
+	if (_has_arrived == false): return
 	if (_invincible == true): return
 	
 	set_invincible(true)
 	tween_hit()
 	reduce_lives()
+
+
+func get_has_arrived() -> bool:
+	return _has_arrived
+
+
+func on_arrive_finished() -> void:
+	hit_box.set_deferred("monitoring", true)
+	_has_arrived = true
 
 
 func _on_trigger_area_entered(_area):
--- bullet/bullet_base/bullet_base.gd
+++ bullet/bullet_base/bullet_base.gd
@@ -11,12 +11,12 @@ func _ready():
 func _process(delta):
 	check_expired(delta)
 	position += _direction * delta
-	
-	
+
+
 func setup(dir: Vector2, life_span: float, speed: float) -> void:
 	_direction = dir.normalized() * speed
 	_life_span = life_span
-	
+
 
 func check_expired(delta: float) -> void:
 	_life_time += delta
@@ -24,5 +24,6 @@ func check_expired(delta: float) -> void:
 		queue_free()
 
 
-func _on_area_entered(_area):
+func _on_area_entered(area):
+	if (area.monitoring == false): return
 	queue_free()
